664b8bb1410737c8c5bf545c54cc1705b66b18e3
Sanjay Mijar <sanjay@kontain.app>
2023-05-19 15:57:07 +0000

    Added a warmup URL for sentiment hugging face demo

--- sentiment/app/main.py
+++ sentiment/app/main.py
@@ -26,5 +26,10 @@ def predict():
     print(hasil[0]['label'])
     return jsonify({'message': content, 'sentiment': hasil[0]['label']})
 
+
+@app.route('/', methods=['GET'])
+def index():
+    return ""
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
