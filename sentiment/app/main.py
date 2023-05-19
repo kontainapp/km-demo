a7b2232a0ff82871a0d6c8d3a870f95ce2759f5c
Serge <serge@kontain.app>
2023-05-19 05:03:36 -1000

    another sentiment demo (#11)

---
+++ sentiment/app/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request  # import objects from the Flask model
+# from keras.models import load_model
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+
+# import torch
+app = Flask(__name__)  # define app using Flask
+
+# tweets = [{'content': 'dont know what todo', 'date': '17-02-2021', 'id': '1'},
+#           {'content': 'doing flask and its pretty great', 'date': '18-02-2021', 'id': '2'}]
+
+# print(keras.__version__)
+# model = load_model('./tf_model.h5')
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english")
+
+pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    content = request.json['content']
+
+    hasil = pipe(content)
+    print(hasil[0]['label'])
+    return jsonify({'message': content, 'sentiment': hasil[0]['label']})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
