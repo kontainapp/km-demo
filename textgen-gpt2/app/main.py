6ebff4d5a210ee423ae25a4f596f69345cc8a960
Sanjay Mijar <sanjay@kontain.app>
2023-05-19 17:15:46 +0000

    GPT2 inference

---
+++ textgen-gpt2/app/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request  # import objects from the Flask model
+# from keras.models import load_model
+#from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+from transformers import pipeline, set_seed
+
+# import torch
+app = Flask(__name__)  # define app using Flask
+
+# print(keras.__version__)
+# model = load_model('./tf_model.h5')
+
+# NO PYTORCH
+generator = pipeline('text-generation', model='gpt2')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    content = request.json['content']
+    set_seed(42)
+    output = generator(content, max_length=30, num_return_sequences=1)
+    print(output)
+
+    return jsonify({'input': content, 'generated_text': output})
+
+@app.route('/', methods=['GET'])
+def index():
+    return ""
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
+# from transformers import GPT2Tokenizer, GPT2Model
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# model = GPT2Model.from_pretrained('gpt2')
+# text = "Once upon a time, "
+# encoded_input = tokenizer(text, return_tensors='pt')
+# output = model(**encoded_input)
