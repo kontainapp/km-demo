cfe99ec8a900452f95973bbea68c68ad5f017b42
Sanjay Mijar <sanjay@kontain.app>
2023-05-23 11:42:59 -0500

    Added mac mps support for matrix add for experimentation of GPU

---
+++ gpu-usage-samples/matrix_add/matrix_add_mac.py
@@ -0,0 +1,38 @@
+# this example just compares the matrix addition time taken on CPU vs GPU
+import torch
+import time
+# temporary workaround for torch.mps (unknown attribute)
+from torch import mps
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+print(f"Using {device} device")
+
+###CPU
+start_time = time.time()
+a = torch.ones(4000,4000, device='cpu')
+for _ in range(100000):
+    a += a
+elapsed_time = time.time() - start_time
+
+print('CPU time = ',elapsed_time)
+
+###GPU
+start_time = time.time()
+if torch.has_cuda:
+    torch.cuda.synchronize()
+elif torch.has_mps:
+    torch.mps.synchronize()
+
+b = torch.ones(4000,4000, device=device) # .cuda()
+for _ in range(100000):
+    b += b
+elapsed_time = time.time() - start_time
+
+print('GPU time = ',elapsed_time)
