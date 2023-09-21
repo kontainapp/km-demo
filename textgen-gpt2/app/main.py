bd52c0f8c64703dba685e5d002c872912429027c
ez-leka <57465356+ez-leka@users.noreply.github.com>
2023-09-21 13:08:22 -0700

    additional endpoints for snapshot and shutdown; configurable port (#19)
    * additional endpoints for snapshot and shutdown; configurable port
    
    * handle snapshot endpoint when there is no libkontain
    
    * formatting

--- textgen-gpt2/app/main.py
+++ textgen-gpt2/app/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify, request  # import objects from the Flask model
+import argparse
+import os
+from ctypes import *
+
 # from keras.models import load_model
-#from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 import torch
+from flask import Flask, jsonify, request
 from transformers import pipeline, set_seed
 
 # import torch
@@ -12,7 +16,11 @@ app = Flask(__name__)  # define app using Flask
 
 # NO PYTORCH
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+print("using device ", device)
+
 generator = pipeline('text-generation', model='gpt2', device=device)
+
 
 @app.route('/predict', methods=['GET'])
 def predict():
@@ -23,12 +31,36 @@ def predict():
 
     return jsonify({'input': content, 'generated_text': output})
 
+
 @app.route('/', methods=['GET'])
 def index():
     return ""
 
+
+@app.get('/shutdown')
+def shutdown():
+    os.kill(os.getpid(), 2)
+    return ('', 204)
+
+
+@app.get('/snapshot')
+def snapshot():
+    try:
+        kontain = CDLL("libkontain.so")
+        kontain.snapshot("pytorch", "text-generation-gp2", 0)
+        return ('', 204)
+    except Exception as e:
+        print("The error is: ", e)
+        return ('', 501)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--port", help="port to run server on",
+                    type=int, default=5000)
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=args.port)
 
 
 # from transformers import GPT2Tokenizer, GPT2Model
